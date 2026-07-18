7057e5f400149c1734247a0f5680e773a7abb68b
Hitarth Gujral <hitarth16102006@gmail.com>
2026-07-18 13:23:20 +0530

    Add Medium Java Solution from LeetCode

--- LeetCode/Java/Medium/Sort an Array/Solution.java
+++ LeetCode/Java/Medium/Sort an Array/Solution.java
@@ -1,47 +1,25 @@
 class Solution {
     public int[] sortArray(int[] arr) {
-        mergeSort(arr, 0, arr.length - 1);
-        return arr;
-    }
 
-    void mergeSort(int[] arr, int l, int r) {
-        if (l >= r)
-            return;
+        int n = arr.length;
 
-        int mid = l + (r - l) / 2;
+        for (int i = 0; i < n - 1; i++) {
 
-        mergeSort(arr, l, mid);
-        mergeSort(arr, mid + 1, r);
+            int l = i;
+            int r = i + 1;
 
-        merge(arr, l, mid, r);
-    }
+            while (r < n) {
 
-    void merge(int[] arr, int l, int mid, int r) {
-        int n1 = mid - l + 1;
-        int n2 = r - mid;
+                if (arr[l] > arr[r]) {
+                    int temp = arr[l];
+                    arr[l] = arr[r];
+                    arr[r] = temp;
+                }
 
-        int[] left = new int[n1];
-        int[] right = new int[n2];
-
-        for (int i = 0; i < n1; i++)
-            left[i] = arr[l + i];
-
-        for (int i = 0; i < n2; i++)
-            right[i] = arr[mid + 1 + i];
-
-        int i = 0, j = 0, k = l;
-
-        while (i < n1 && j < n2) {
-            if (left[i] <= right[j])
-                arr[k++] = left[i++];
-            else
-                arr[k++] = right[j++];
+                r++;
+            }
         }
 
-        while (i < n1)
-            arr[k++] = left[i++];
-
-        while (j < n2)
-            arr[k++] = right[j++];
+        return arr;
     }
 }
