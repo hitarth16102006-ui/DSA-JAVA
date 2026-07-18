385e67c159952789d3456b367622da390f30bace
Hitarth Gujral <hitarth16102006@gmail.com>
2026-07-18 23:52:00 +0530

    Add Easy Java Solution from LeetCode

--- LeetCode/Java/Easy/Third Maximum Number/Solution.java
+++ LeetCode/Java/Easy/Third Maximum Number/Solution.java
@@ -1,34 +1,46 @@
 class Solution {
     public int thirdMax(int[] nums) {
-        int tmax_found=0;
-        int max=0;
-        int smax=-1;
-        int tmax=Integer.MIN_VALUE;
 
-        for(int i=0;i<nums.length;i++){
-            if(nums[i]>max){
-                max=nums[i];
+        int max = Integer.MIN_VALUE;
+        int smax = Integer.MIN_VALUE;
+        int tmax = Integer.MIN_VALUE;
+
+        // Find maximum
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] > max) {
+                max = nums[i];
             }
         }
 
-        for(int i=0;i<nums.length;i++){
-            if(nums[i]>smax && nums[i]<max){
-                smax=nums[i];
+        // Find second maximum
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != max && nums[i] > smax) {
+                smax = nums[i];
             }
         }
 
-        for(int i=0;i<nums.length;i++){
-            if(nums[i]>=tmax && nums[i]<smax){
-                tmax=nums[i];
-                tmax_found++;
+        // Find third maximum
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != max && nums[i] != smax && nums[i] > tmax) {
+                tmax = nums[i];
             }
         }
 
-        if(tmax_found>0){
-            return tmax;
-        }else{
+        // Count distinct numbers
+        int count = 0;
+
+        if (max != Integer.MIN_VALUE)
+            count++;
+
+        if (smax != Integer.MIN_VALUE)
+            count++;
+
+        if (tmax != Integer.MIN_VALUE)
+            count++;
+
+        if (count < 3)
             return max;
-        }
-    }
 
+        return tmax;
+    }
 }
