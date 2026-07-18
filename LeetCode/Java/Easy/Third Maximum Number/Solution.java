9054599f710b3aa990e35d55424ccdf9ec99dd78
Hitarth Gujral <hitarth16102006@gmail.com>
2026-07-18 23:44:10 +0530

    Add Easy Java Solution from LeetCode

--- LeetCode/Java/Easy/Third Maximum Number/Solution.java
+++ LeetCode/Java/Easy/Third Maximum Number/Solution.java
@@ -2,7 +2,7 @@ class Solution {
     public int thirdMax(int[] nums) {
         int max=0;
         int smax=-1;
-        int tmax=-2;
+        int tmax=INT.;
 
         for(int i=0;i<nums.length;i++){
             if(nums[i]>max){
