0f43aa44018fc5f744f56b8b6a08cba34b003826
Hitarth Gujral <hitarth16102006@gmail.com>
2026-08-18 14:50:26 +0530

    Add Java Solution to Patterns/Binary Search

--- LeetCode/Patterns/Binary Search/162. Find Peak Element/Solution.java
+++ LeetCode/Patterns/Binary Search/162. Find Peak Element/Solution.java
@@ -3,9 +3,8 @@ class Solution {
         int num=0;
         for(int i=0;i<nums.length-2;i++){
             if(nums[i]<nums[i+1] && nums[i+1]>nums[i+2]){
-                num=nums[i];
-            }else 
-            num=nums[0];
+                num=i+1;
+            }
         }
         return num;
     }
