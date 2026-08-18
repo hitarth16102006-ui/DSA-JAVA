961a2eaacd198dd7540a11a914951103be6ffeef
Hitarth Gujral <hitarth16102006@gmail.com>
2026-08-18 14:46:46 +0530

    Add Medium Java Solution from LeetCode

--- LeetCode/Java/Medium/162. Find Peak Element/Solution.java
+++ LeetCode/Java/Medium/162. Find Peak Element/Solution.java
@@ -1,10 +1,11 @@
 class Solution {
     public int findPeakElement(int[] nums) {
-        int num;
+        int num=0;
         for(int i=0;i<nums.length-2;i++){
-            if(nums[i]<nums[i+1] && nums[i+1]<nums[i+2]){
-                num=nums[i+1];
-            }
+            if(nums[i]<nums[i+1] && nums[i+1]>nums[i+2]){
+                num=nums[i];
+            }else 
+            num=nums[0];
         }
         return num;
     }
